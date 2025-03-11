f4af5ee4505c4e8612b17e17123941934040943a
Diamond Land <81316675+DiamondLand@users.noreply.github.com>
2025-03-11 11:34:15 +0300

    conftest

---
+++ backend/tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database.models import Base
+from fastapi.testclient import TestClient
+from cli import app
+from core.config import get_db
+
+TEST_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Создание тестовой БД
+@pytest.fixture(scope="function")
+def db():
+    Base.metadata.create_all(bind=engine)
+    session = TestingSessionLocal()
+    yield session
+    session.close()
+    Base.metadata.drop_all(bind=engine)
+
+# Подменяем зависимость get_db в FastAPI
+@pytest.fixture(scope="function")
+def client(db):
+    def override_get_db():
+        yield db
+
+    app.dependency_overrides[get_db] = override_get_db
+    return TestClient(app)
